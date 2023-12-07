88b959fdbe1fdaad025f5e7b80e101708a7f7782
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2023-12-07 21:57:24 +0100

    07-12-2023

--- Client/LundBot69 Client/MVVM/View/SrMainView.xaml.cs
+++ Client/LundBot69 Client/MVVM/View/SrMainView.xaml.cs
@@ -24,5 +24,41 @@ namespace LundBot69_Client.MVVM.View
 		{
 			InitializeComponent();
 		}
+
+		private void ListView_Loaded(object sender, RoutedEventArgs e)
+		{
+			ListView listView = (ListView)sender;
+			GridView gridView = listView.View as GridView;
+
+			if (gridView != null)
+			{
+				double totalWidth = listView.ActualWidth;
+
+				// Set proportional widths
+				gridView.Columns[0].Width = totalWidth * 0.25; // Username column
+				gridView.Columns[1].Width = totalWidth * 0.5; // Points column
+				gridView.Columns[2].Width = totalWidth * 0.25; // Apply column
+			}
+		}
+
+		private void ApplyButton(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ApplyButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void DeleteButton_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void RefreshDataButton(object sender, RoutedEventArgs e)
+		{
+
+		}
 	}
 }
