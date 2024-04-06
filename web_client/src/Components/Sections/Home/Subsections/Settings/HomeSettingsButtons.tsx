2e7197ec56b1e9e2695d23f169d1e549d44b0fea
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2024-04-06 14:37:03 +0200

    06-04-2024
    Attending birthday, ran out of time for the day.

--- web_client/src/Components/Sections/Home/Subsections/Settings/HomeSettingsButtons.tsx
+++ web_client/src/Components/Sections/Home/Subsections/Settings/HomeSettingsButtons.tsx
@@ -1,40 +1,66 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './HomeSettingsButtons.scss'
 import Button from '../../../../Inputs/Button'
-import InputForm from '../../../../Content/InputForm'
 
 const HomeSettingsButtons: React.FC = () => {
   // TODO: Replace onClick events with something useful
-  // TODO: UseState or useReducer to handle the button text
-  // TODO: Make the buttons same width and height always
+
+  // TODO: Use global state instead, to prevent losing these states on rerender.
+  const [botEnabled, setBotEnabled] = useState(false)
+  const [songRequestEnabled, setSongRequestEnabled] = useState(false)
+  const [gamblingEnabled, setGamblingEnabled] = useState(false)
+  const [autoShoutOutEnabled, setAutoShoutOutEnabled] = useState(false)
+
+  // TODO: Get settings values from database
+
+  // TODO: Call database, to perform changes, for all the click events
+  const botSettingClicked = () => {
+    setBotEnabled(!botEnabled)
+    console.log(botEnabled)
+  }
+
+  const songRequestSettingClicked = () => {
+    setSongRequestEnabled(!songRequestEnabled)
+    console.log(songRequestEnabled)
+  }
+
+  const gamblingSettingClicked = () => {
+    setGamblingEnabled(!gamblingEnabled)
+    console.log(gamblingEnabled)
+  }
+
+  const shoutOutSettingClicked = () => {
+    setAutoShoutOutEnabled(!autoShoutOutEnabled)
+    console.log(autoShoutOutEnabled)
+  }
 
   return (
     <>
       <Button
         className="settings__button--bot"
         hint="Enables/disables LundBot69 on your twitch channel."
-        onClick={() => console.log('lundbot69')}
+        onClick={botSettingClicked}
       >
         Enable LundBot69
       </Button>
       <Button
         className="settings__button--song-request"
         hint="Enables/disables song requests."
-        onClick={() => console.log('song request')}
+        onClick={songRequestSettingClicked}
       >
         Enable song request
       </Button>
       <Button
         className="settings__button--gambling"
         hint="Enables/disables gambling commands."
-        onClick={() => console.log('gambling')}
+        onClick={gamblingSettingClicked}
       >
         Enable gambling
       </Button>
       <Button
         className="settings__button--shoutout"
         hint="Enables/disables automatic shoutout of raiding channels."
-        onClick={() => console.log('gambling')}
+        onClick={gamblingSettingClicked}
       >
         Enable auto shout-out
       </Button>
