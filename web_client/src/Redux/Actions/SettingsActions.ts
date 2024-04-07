33d5f34dffc20d6f448ce9e1f8e5149e5b53914a
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2024-04-07 21:59:42 +0200

    07-04-2024

--- web_client/src/Redux/Actions/SettingsActions.ts
+++ web_client/src/Redux/Actions/SettingsActions.ts
@@ -7,3 +7,28 @@ export const setEffectsVolume = (volume: number) => ({
   type: 'SET_EFFECTS_VOLUME',
   payload: volume,
 })
+
+export const setPinnedMessage = (message: string) => ({
+  type: 'SET_PINNED_MESSAGE',
+  payload: message,
+})
+
+export const setBotIsEnabled = (isEnabled: boolean) => ({
+  type: 'SET_BOT_ENABLED',
+  payload: isEnabled,
+})
+
+export const setSongRequestIsEnabled = (isEnabled: boolean) => ({
+  type: 'SET_SONG_REQUEST_ENABLED',
+  payload: isEnabled,
+})
+
+export const setGamblingIsEnabled = (isEnabled: boolean) => ({
+  type: 'SET_GAMBLING_ENABLED',
+  payload: isEnabled,
+})
+
+export const setAutoShoutOutIsEnabled = (isEnabled: boolean) => ({
+  type: 'SET_AUTO_SHOUTOUT_ENABLED',
+  payload: isEnabled,
+})
--- web_client/src/Redux/Reducers/SettingsReducer.ts
+++ web_client/src/Redux/Reducers/SettingsReducer.ts
@@ -2,10 +2,22 @@ import { IAction } from '../IAction'
 
 interface ISettingsState {
   musicVolume: number
+  effectsVolume: number
+  pinnedMessage: string
+  botEnabled: boolean
+  songRequestEnabled: boolean
+  gamblingEnabled: boolean
+  autoShoutOutEnabled: boolean
 }
 
 const initialState: ISettingsState = {
   musicVolume: 50,
+  effectsVolume: 50,
+  pinnedMessage: '',
+  botEnabled: false,
+  songRequestEnabled: false,
+  gamblingEnabled: false,
+  autoShoutOutEnabled: false,
 }
 
 const settingsReducer = (state = initialState, action: IAction) => {
@@ -16,7 +28,35 @@ const settingsReducer = (state = initialState, action: IAction) => {
         musicVolume: action.payload,
       }
     case 'SET_EFFECTS_VOLUME':
-      return {}
+      return {
+        ...state,
+        effectsVolume: action.payload,
+      }
+    case 'SET_PINNED_MESSAGE':
+      return {
+        ...state,
+        pinnedMessage: action.payload,
+      }
+    case 'SET_BOT_ENABLED':
+      return {
+        ...state,
+        botEnabled: action.payload,
+      }
+    case 'SET_SONG_REQUEST_ENABLED':
+      return {
+        ...state,
+        songRequestEnabled: action.payload,
+      }
+    case 'SET_GAMBLING_ENABLED':
+      return {
+        ...state,
+        gamblingEnabled: action.payload,
+      }
+    case 'SET_AUTO_SHOUTOUT_ENABLED':
+      return {
+        ...state,
+        autoShoutOutEnabled: action.payload,
+      }
     default:
       return state
   }
